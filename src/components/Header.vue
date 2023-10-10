d7c2065bc9e1b40ee83a99fdf1da4fb332dbd17f
GiacWebDev <giacobechini@gmail.com>
2023-10-10 16:41:40 +0200

    [edit] scss header [add] as * to import scss

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,13 +8,59 @@ export default {
 
 <template>
 
-  <h1>questo è l'header</h1>
+<header>
+  <div class="left debug">
+    <div class="menu">
+      <ul>
+        <li><a href="#">Donna</a></li>
+        <li><a href="#">Uomo</a></li>
+        <li><a href="#">Bambini</a></li>
+      </ul>
+    </div>
+  </div>
+  
+  <div class="logo">
+    <img src="/public/img/boolean-logo.png" alt="logo">
+  </div>
+
+  
+  <div class="right">
+    <div class="icons">
+      icona
+      icona
+      
+    </div>
+  </div>
+  
+</header>
+
   
 </template>
 
 
 
-<style>
+<style lang="scss" scoped>
+@use '../SCSS/main' as *;
+
+header {
+  display: flex;
+  align-items: center;
+  padding: 10px 0;
+  background-color: $primary-color;
+}
+
+.logo img {
+  width: 150px;
+}
+
+ul li {
+  display: inline-block;
+  padding: 5px;
+}
+
+a {
+  color: $text-secondary-color;
+}
 
 
 </style>
